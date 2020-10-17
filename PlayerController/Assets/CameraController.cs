7693e648fba0d155990080d56e45222542c3a39c
isip_d.a.pahomov@mpt.ru <isip_d.a.pahomov@mpt.ru>
2020-10-18 02:22:18 +0300

    .

---
+++ PlayerController/Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    // объект для возможности управления камеры при трансформации
+    public Vector3 offset; // векторные координаты
+
+    public bool useOffsetValues; // от какого лица
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!useOffsetValues)
+        { // если не от первого лица
+            offset = target.position - transform.position; // вычитать начальные значения камеры от положения на экране
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void LateUpdate() // более позднее изменение не 
+    {
+        transform.position = target.position - offset; // измение положения камеры
+        transform.LookAt(target); // просмотр передижения 
+    }
+}
+
+
---
+++ PlayerController/Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;// Создание скорости передвижения
+    public float jumpForce; //Создание высоты прыжков
+    public Vector3 moveDirection;
+    public float gravityScale = 5f;
+    public CharacterController charController;
+
+    private Camera theCam;
+    public GameObject playerModel;
+    public float rotateSpeed;
+
+    private void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        theCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        float yStore = moveDirection.y;
+
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDirection.Normalize();
+        moveDirection = moveDirection * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (charController.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        charController.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, theCam.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+    }
+
+}
+
+
